4f22a397822a168ae04be49bbd0c442a06fd85d4
JiahaoZhu11 <jiahao_zhu@umail.ucsb.edu>
2020-02-19 02:56:03 -0800

    version 1.0.1

---
+++ Invoice Divider/Invoice DividerDlg.h
@@ -0,0 +1,46 @@
+﻿
+// Invoice DividerDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CInvoiceDividerDlg 对话框
+class CInvoiceDividerDlg : public CDialogEx
+{
+// 构造
+public:
+	CInvoiceDividerDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_INVOICEDIVIDER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnEnChangename();
+	afx_msg void OnEnChangeuprice();
+	afx_msg void OnEnChangetprice();
+	afx_msg void OnEnChangenum();
+	afx_msg void OnBnClickedadd();
+	CListCtrl initialList;
+	afx_msg void OnLvnItemchangedinput(NMHDR* pNMHDR, LRESULT* pResult);
+	CListCtrl finalList;
+	afx_msg void OnBnClickeddivide();
+	afx_msg void OnBnClickeddel();
+	afx_msg void OnBnClickedclean();
+};
---
+++ Invoice Divider/InvoiceLST.h
@@ -0,0 +1,43 @@
+//InvoiceLST.h
+//Author: Jiahao Zhu
+//List of invoice header file
+
+#pragma once
+class InvoiceLST
+{
+public:
+	InvoiceLST();
+	InvoiceLST(const InvoiceLST &source);
+	~InvoiceLST();
+	BOOL isEmpty();
+	int getInvoice();
+	CString getName();
+	double getUprice();
+	double getTprice();
+	int getNum();
+	void removeFirst();
+	void append(int invoice, CString iName, double iUprice, double iTprice, int iNum);
+	void remove(int invoice);
+
+	CString test();
+
+private:
+	void clear();
+	void copy(const InvoiceLST &source);
+
+	struct Invoice
+	{
+		int invoice;
+		CString iName;
+		double iUprice;
+		double iTprice;
+		int iNum;
+		Invoice* next;
+	};
+
+	Invoice* first;
+};
+
+/*void divisionTest(InvoiceLST iList, InvoiceLST fList);
+
+InvoiceLST division(InvoiceLST iList);*/
